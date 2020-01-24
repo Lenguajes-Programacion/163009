eaa135f437b533baeb7562b8c0b685376bbc169f
Eli R. Diaz <60105277+EliManson@users.noreply.github.com>
2020-01-24 12:07:57 -0700

    Practica_Uno

---
+++ Clase_uno/Practica_uno/Practica_uno/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practica_uno
+{
+    class Program
+    {
+        string nombre = "Asdfghjkl";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There " + this.nombre);
+            Console.ReadKey();
+        }
+    }
+}
