5aa83cd1c8c3d96866b82007ef5837e766b977cb
Eli R. Diaz <60105277+EliManson@users.noreply.github.com>
2020-02-07 12:48:13 -0700

    Epic Gamer Moment

---
+++ Clase_uno/Prueba2/Practica2/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ Nombre: Jesús Eli Roemro Díaz.
+ Materia: Lenguajes de Programacion I
+ Practica: Calculadora
+ */
+using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float NumeroUno;
+            float NumeroDos;
+            float Res;
+            int Principal;
+            do
+            {
+                Console.WriteLine("Bienvenido a la calculadora B.I.G  Y.O.S.H.I!");
+                Console.WriteLine("1° Suma" + "\n" + "2° Resta" + "\n" + "3° Multiplicacion" + "\n" + "4° Division" + "\n" + "5° Salir" + "\n");
+                Principal = Int32.Parse(Console.ReadLine());
+                switch (Principal)
+                {
+                    case 1:
+                        Console.WriteLine("¿Que quieres sumar Lmao? \n");
+                        Console.WriteLine("Primer Numero: ");
+                        NumeroUno = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Segundo Numero: ");
+                        NumeroDos = Int32.Parse(Console.ReadLine());
+                        Res = NumeroUno + NumeroDos;
+                        Console.WriteLine("Su resultado es: " + Res);
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("Es una Resta Lmao");
+                        Console.WriteLine("Primer Numero: ");
+                        NumeroUno = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Segundo Numero: ");
+                        NumeroDos = Int32.Parse(Console.ReadLine());
+                        Res = NumeroUno - NumeroDos;
+                        Console.WriteLine("Su resultado es: " + Res);
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Es una Multiplicacion Lmao");
+                        Console.WriteLine("Primer Numero: ");
+                        NumeroUno = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Segundo Numero: ");
+                        NumeroDos = Int32.Parse(Console.ReadLine());
+                        Res = NumeroUno * NumeroDos;
+                        Console.WriteLine("Su resultado es: " + Res);
+                        Console.WriteLine("\n");
+                        break;
+                    case 4:
+                        Console.WriteLine("Es una Division Lmao");
+                        Console.WriteLine("Primer Numero: ");
+                        NumeroUno = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Segundo Numero: ");
+                        NumeroDos = Int32.Parse(Console.ReadLine());
+                        Res = NumeroUno / NumeroDos;
+                        Console.WriteLine("Su resultado es: " + Res);
+                        Console.WriteLine("\n");
+                        break;
+                    case 5:
+                        Console.WriteLine("Hasta la Proxima asdfghjkl!!!");
+                        Console.WriteLine("\n");
+                        break;
+                    default:
+                        Console.WriteLine("Los argumentos no tienen sentido Lmao");
+                        Console.WriteLine("\n");
+                        break;
+                }
+            } while (Principal != 5);
+            Console.ReadKey();
+        }
+    }
+}
