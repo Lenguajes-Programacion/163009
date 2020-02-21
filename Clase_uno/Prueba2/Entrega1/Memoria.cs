20aac1b4f862c99af427574f8391be8d446f5513
Eli R. Diaz <60105277+EliManson@users.noreply.github.com>
2020-02-21 11:43:10 -0700

    Mi loco

---
+++ Clase_uno/Prueba2/Entrega1/Memoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Entrega1
+{
+    class Memoria
+    {
+        public void Guardar()
+        {
+            string filepath = "../db.json";
+            StreamReader reader = new StreamReader(filepath);
+            var dbJSON = reader.ReadToEnd();
+            var dbObject = JObject.Parse(dbJSON);
+        }
+    }
+}
